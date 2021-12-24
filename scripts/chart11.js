c9c37af337af84956192a8d431e73c5c806b00f3
eryk-tabis <s222765@student.uek.krakow.pl>
2021-12-24 11:37:32 +0100

    working charts+ i find clue on how to display between divs(can do some 'subsites' in one html file example in test.html)

--- scripts/chart11.js
+++ scripts/chart11.js
@@ -1,41 +1,40 @@
 //temperature fron december every day linear
-
-function get_data(){
-    var xmlhttp = new XMLHttpRequest();
-    //link to api here
-    var url = "https://my.api.mockaroo.com/data.json?key=a2d938f0";
-    //nie wiem co to ale do sprawdzenia
-    // xmlhttp.onreadystatechange = function() {
-    //     if (this.readyState == 4 && this.status == 200) {
-    //         var myArr = JSON.parse(this.responseText);
+// function get_data(){
+//     var xmlhttp = new XMLHttpRequest();
+//     //link to api here
+//     var url = "https://my.api.mockaroo.com/data.json?key=a2d938f0";
+//     //nie wiem co to ale do sprawdzenia
+//     // xmlhttp.onreadystatechange = function() {
+//     //     if (this.readyState == 4 && this.status == 200) {
+//     //         var myArr = JSON.parse(this.responseText);
             
-    //     }
-    // };
-    xmlhttp.open("GET", url, false);
-    xmlhttp.send();
-    return JSON.parse(xmlhttp.responseText)
-    }
+//     //     }
+//     // };
+//     xmlhttp.open("GET", url, false);
+//     xmlhttp.send();
+//     return JSON.parse(xmlhttp.responseText)
+//     }
     
-function get_data_by_month(data, month){
-    new_data = []
-    for(i = 0; i< 100; i++){
-         if(data[i]["day_by_day"].substring(5,7) == month){
+// function get_data_by_month(data, month){
+//     new_data = []
+//     for(i = 0; i< 100; i++){
+//          if(data[i]["day_by_day"].substring(5,7) == month){
              
-             new_data.push(data[i]["day_by_day"].slice(0,11));
-         }
-    }
-    return new_data;
-}
-function get_temp_by_month(data, month){
-    new_data = []
-    for(i = 0; i< 100; i++){
-         if(data[i]["day_by_day"].substring(5,7) == month){
+//              new_data.push(data[i]["day_by_day"].slice(0,11));
+//          }
+//     }
+//     return new_data;
+// }
+// function get_temp_by_month(data, month){
+//     new_data = []
+//     for(i = 0; i< 100; i++){
+//          if(data[i]["day_by_day"].substring(5,7) == month){
              
-             new_data.push(data[i]["temperature"]);
-         }
-    }
-    return new_data;
-}
+//              new_data.push(data[i]["temperature"]);
+//          }
+//     }
+//     return new_data;
+// }
 
 
 all_data = get_data();
--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-export function get_data_by_month(data, month){
+function get_data_by_month(data, month){
     for(i = 1; i<= 100; i++){
         if(data[i][1].substring(5,6) == month)
             return data[i];
