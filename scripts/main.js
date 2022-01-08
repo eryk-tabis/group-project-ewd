1474794df196209faddb77e234ac332815b55271
eryk-tabis <s222765@student.uek.krakow.pl>
2022-01-08 20:24:07 +0100

    UPDATE

--- scripts/main.js
+++ scripts/main.js
@@ -178,9 +178,50 @@ function chart6(data) {
     });
 }
 
+function addTable(data){
+    var col = [];
+        for (var i = 0; i < data.length; i++) {
+            for (var key in data[i]) {
+                if (col.indexOf(key) === -1) {
+                    col.push(key);
+                }
+            }
+        }
+
+        // CREATE DYNAMIC TABLE.
+        var table = document.createElement("table");
+
+        // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+        var tr = table.insertRow(-1);                   // TABLE ROW.
+
+        for (var i = 0; i < col.length; i++) {
+            var th = document.createElement("th");      // TABLE HEADER.
+            th.innerHTML = col[i];
+            tr.appendChild(th);
+        }
+
+        // ADD JSON DATA TO THE TABLE AS ROWS.
+        for (var i = 0; i < data.length; i++) {
+
+            tr = table.insertRow(-1);
+
+            for (var j = 0; j < col.length; j++) {
+                var tabCell = tr.insertCell(-1);
+                tabCell.innerHTML = data[i][col[j]];
+            }
+        }
+
+        // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+        var divContainer = document.getElementById("table");
+        divContainer.innerHTML = "";
+        divContainer.appendChild(table);
+}
+
 async function get_data() {
     const response = await fetch("https://my.api.mockaroo.com/data.json?key=a2d938f0");
     const data = await response.json();
+    addTable(data);
     chart1(data);
     chart2(data);
     chart3(data);
